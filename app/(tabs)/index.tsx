9c1442007fb8001f1d5c600aec0c3fbed5ce1fa8
Iyasya03 <ahiyasya@gmail.com>
2024-12-01 11:26:10 +0700

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,49 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Image } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+class MateriFlexBox extends Component {
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#c8d6e5',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <View style={{ backgroundColor: '#ee5253', width: 100, height: 100 }} />
+          <View style={{ backgroundColor: '#feca57', width: 100, height: 100 }} />
+          <View style={{ backgroundColor: '#1dd1a1', width: 100, height: 100 }} />
+          <View style={{ backgroundColor: '#5f27cd', width: 100, height: 100 }} />
+        </View>
+ 
+        <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginTop: 20 }}>
+          <Text style={{ marginRight: 10 }}>Beranda</Text>
+          <Text style={{ marginRight: 10 }}>Vidio</Text>
+          <Text style={{ marginRight: 10 }}>Playlist</Text>
+          <Text style={{ marginRight: 10 }}>Komunitas</Text>
+          <Text style={{ marginRight: 10 }}>Chanel</Text>
+          <Text style={{ marginRight: 10 }}>Tentang</Text>
+        </View>
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+
+        <View style={{ flexDirection: 'row', alignItems: 'center',justifyContent: 'center', marginTop: 20 }}>
+          <Image
+            source={{
+              uri: 'https://c4.wallpaperflare.com/wallpaper/214/333/302/bring-me-the-horizon-that-s-the-spirit-logo-wallpaper-preview.jpg',
+            }}
+            style={{ width: 100, height: 100, borderRadius: 50, marginRight: 14 }}
+          />
+          <View>
+            <Text style={{ fontSize: 20, fontWeight: 'bold',color:'blue' }}>Ahmad iyasya</Text>
+            <Text>1 JT subscribe</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default MateriFlexBox;
